f804391bcd91a80a16e7aff65399a79b10f613de
mayiiexvi <espinasylvia16@gmail.com>
2023-11-27 11:14:13 -0500

    add search book api

--- controllers/book.js
+++ controllers/book.js
@@ -5,6 +5,7 @@ const BookData = require('../models/bookdata.js');
 const isAdmin = require('../middleware/isAdmin'); 
 
 
+//Getting list of books
 const getBooks = async (req, res) => {
     try {
         const books = await BookData.find();
@@ -15,6 +16,7 @@ const getBooks = async (req, res) => {
     }
 };
 
+//Getting book by ID
 const getBookById = async (req, res) => {
     try {
         const boookId = req.query.bookId;
@@ -29,6 +31,25 @@ const getBookById = async (req, res) => {
     }
 };
 
+//Searching book by title, author or genre
+const searchBooks = async (req, res) => {
+    try {
+        const query = req.params.query;
+        const books = await BookData.find({
+            $or: [
+                { title: { $regex: query, $options: "i" } }, // Case-insensitive title search
+                { author: { $regex: query, $options: "i" } }, // Case-insensitive author search
+                { genre: { $regex: query, $options: "i" } }, // Case-insensitive genre search
+            ],
+        });
+
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+//Adding book
 const addBook = async (req, res) => {
     const { bookId, title, author, genre, price } = req.body;
 
@@ -48,6 +69,7 @@ const addBook = async (req, res) => {
     }
 };
 
+//Updating book
 const updateBook = async (req, res) => {
     const bookId = req.params.title;
     const { title, author, genre, price } = req.body;
@@ -69,6 +91,7 @@ const updateBook = async (req, res) => {
     }
 };
 
+//Deleting book
 const deleteBook = async (req, res) => {
     const bookId = req.params.bookId;
 
@@ -87,6 +110,7 @@ const deleteBook = async (req, res) => {
 
 module.exports.getBooks = getBooks;
 module.exports.getBookById = getBookById;
+module.exports.searchBooks = searchBooks;
 module.exports.addBook = addBook;
 module.exports.updateBook = updateBook;
 module.exports.deleteBook = deleteBook;
--- routes/book.js
+++ routes/book.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get('/getBooks', book.getBooks);
 router.get('/getBook/:bookId', book.getBookById);
+router.get("/searchBook/:query", book.searchBooks);
 router.post('/addBook', isAdmin, book.addBook);
 router.put('/updateBook/:bookId', isAdmin, book.updateBook);
 router.delete('/deleteBook/:bookId', isAdmin, book.deleteBook);
