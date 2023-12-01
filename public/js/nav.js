8349262ad2f7eada4bff37d76c118c345652b409
Mufida A <49670663+mufidaandi@users.noreply.github.com>
2023-12-01 10:15:35 -0500

    search function

--- public/js/nav.js
+++ public/js/nav.js
@@ -27,54 +27,6 @@ function unhideBackground() {
   $("#content-wrapper").css("filter", "brightness(1.0)");
 }
 
-function setCatActiveCategory() {
-    $("#cat").addClass("active-sub");
-    $("#dog").removeClass("active-sub");
-    $("#bird").removeClass("active-sub");
-  
-    $("#food").attr("src", "images/catfood.png");
-    $("#treat").attr("src", "images/cattreat.png");
-    $("#supplies").attr("src", "images/catsupplies.png");
-
-    $("#sub-food").attr("href", "./product-list.html?category=cat_food");
-    $("#sub-treat").attr("href", "./product-list.html?category=cat_treat");
-    $("#sub-supply").attr("href", "./product-list.html?category=cat_supply");
-  
-    $("#category-image").attr("src", "images/catto.png");
-  }
-  
-  function setDogActiveCategory() {
-    $("#cat").removeClass("active-sub");
-    $("#dog").addClass("active-sub");
-    $("#bird").removeClass("active-sub");
-  
-    $("#food").attr("src", "images/dogfood.png");
-    $("#treat").attr("src", "images/dogtreats.png");
-    $("#supplies").attr("src", "images/dogsupplies.png");
-  
-    $("#sub-food").attr("href", "./product-list.html?category=dog_food");
-    $("#sub-treat").attr("href", "./product-list.html?category=dog_treat");
-    $("#sub-supply").attr("href", "./product-list.html?category=dog_supply");
-
-    $("#category-image").attr("src", "images/doggo.png");
-  }
-  
-  function setBirdActiveCategory() {
-    $("#cat").removeClass("active-sub");
-    $("#dog").removeClass("active-sub");
-    $("#bird").addClass("active-sub");
-  
-    $("#food").attr("src", "images/birdfood.png");
-    $("#treat").attr("src", "images/birdtreats.png");
-    $("#supplies").attr("src", "images/birdsupplies.png");
-  
-    $("#sub-food").attr("href", "./product-list.html?category=bird_food");
-    $("#sub-treat").attr("href", "./product-list.html?category=bird_treat");
-    $("#sub-supply").attr("href", "./product-list.html?category=bird_supply");
-
-    $("#category-image").attr("src", "images/birdo.png");
-  }
-
   function userIconOnHover() {
     $("#user-icon").css("filter", "brightness(1.3)");
     $("#content-wrapper").css("filter", "brightness(0.5)");
@@ -110,18 +62,14 @@ function setCatActiveCategory() {
     $("#cart-wrapper").hover(cartIconOnHover, cartIconOnLeave);
   
     $("#login-btn").hover(loginBtnOnHover, loginBtnOnLeave);
-  
-    $("#cat").mouseover(setCatActiveCategory);
-    $("#dog").mouseover(setDogActiveCategory);
-    $("#bird").mouseover(setBirdActiveCategory);
-  
+
     $("#search-input").blur(expandSearch);
     $("#search").click(expandSearch);
     $("#products, #products-modal").hover(hideBackground, unhideBackground);
 
     $(".search-bar").submit(function( event ) {
       const keyword = $("#search-input").first().val();
-      window.location.href = "product-search.html?search=" + keyword;
+      window.location.href = "books-list.html?search=" + keyword;
       event.preventDefault();
     });
   });
