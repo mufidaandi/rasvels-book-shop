0895e50d8fc36f0e6d7b3e0520c90bdc0a80f244
tichvu2611 <tichvu2611@gmail.com>
2023-12-08 10:49:29 -0500

    making ejs

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 // Home page
+// Home page
+const redirectHome = (req, res, next) => {
+    res.redirect('/home');
+  };
+router.get('/', redirectHome);
 router.get('/home', userController.dashboard);
-
+router.get('/book-list', userController.bookList);
 
 module.exports = router;
